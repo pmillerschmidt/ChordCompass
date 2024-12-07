b9471405c3515324233626bec6b86dde4fbc1472
paulmillerschmidt <paul.miller-schmidt@yale.edu>
2024-12-07 14:35:00 -0500

    updates

--- backend/main.py
+++ backend/main.py
@@ -17,7 +17,7 @@ app = FastAPI()
 # Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://pmillerschmidt.github.io/ChordCompass/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
